f073d0d3c1ebad79d42068b3203696067fae5b11
Juan David Aristizábal <jd.ac@outlook.com>
2017-04-18 06:47:30 -0500

    implement dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,14 @@
 class PagesController < ApplicationController
+  before_action :authenticate_user!
   def home
+    if current_user
+      redirect_to dashboard_path
+    end
+  end
+
+  def dashboard
+    @user = current_user
+    @places = @user.places
+    @reviews = @user.reviews
   end
 end
